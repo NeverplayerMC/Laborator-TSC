ea752b04f0a8601fa7850b0994fb8ab7edf43d9f
ionut <neverplayermc@gmail.com>
2022-03-15 22:10:08 +0200

    Commit_from_Shell

--- lab2Rezolvat/lab01_testbench-interface/top.sv
+++ lab2Rezolvat/lab01_testbench-interface/top.sv
@@ -3,7 +3,7 @@
  **********************************************************************/
 
 module top;
-  timeunit 1ns/1ns;
+//  timeunit 1ns/1ns;
 
   // user-defined types are defined in instr_register_pkg.sv
   import instr_register_pkg::*;
@@ -12,38 +12,40 @@ module top;
   logic clk;
   logic test_clk;
 
+  tb_ifc myio (.clk(test_clk));
   // interconnecting signals
-  logic          load_en;
-  logic          reset_n;
-  opcode_t       opcode;
-  operand_t      operand_a, operand_b;
-  address_t      write_pointer, read_pointer;
-  instruction_t  instruction_word;
+  //logic          load_en;
+  //logic          reset_n;
+  //opcode_t       opcode;
+  //operand_t      operand_a, operand_b;
+  //address_t      write_pointer, read_pointer;
+  //instruction_t  instruction_word;
 
+  instr_register_test test (.myio(myio));
   // instantiate testbench and connect ports
-  instr_register_test test (
-    .clk(test_clk),
-    .load_en(load_en),
-    .reset_n(reset_n),
-    .operand_a(operand_a),
-    .operand_b(operand_b),
-    .opcode(opcode),
-    .write_pointer(write_pointer),
-    .read_pointer(read_pointer),
-    .instruction_word(instruction_word)
-   );
+  //instr_register_test test (
+   // .clk(test_clk),
+   // .load_en(load_en),
+   // .reset_n(reset_n),
+   // .operand_a(operand_a),
+   // .operand_b(operand_b),
+   // .opcode(opcode),
+   // .write_pointer(write_pointer),
+   // .read_pointer(read_pointer),
+    //.instruction_word(instruction_word)
+  // );
 
   // instantiate design and connect ports
   instr_register dut (
     .clk(clk),
-    .load_en(load_en),
-    .reset_n(reset_n),
-    .operand_a(operand_a),
-    .operand_b(operand_b),
-    .opcode(opcode),
-    .write_pointer(write_pointer),
-    .read_pointer(read_pointer),
-    .instruction_word(instruction_word)
+    .load_en(myio.load_en),
+    .reset_n(myio.reset_n),
+    .operand_a(myio.operand_a),
+    .operand_b(myio.operand_b),
+    .opcode(myio.opcode),
+    .write_pointer(myio.write_pointer),
+    .read_pointer(myio.read_pointer),
+    .instruction_word(myio.instruction_word)
    );
 
   // clock oscillators
